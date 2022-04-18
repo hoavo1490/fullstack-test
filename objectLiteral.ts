28594019e1e28dcb6929cdb2be3954cc7883c5b1
Hoa Vo <hoavo1490@gmail.com>
2022-04-18 19:55:24 +0700

    objectLiteral: Change type of result value object

--- objectLiteral.ts
+++ objectLiteral.ts
@@ -1,9 +1,9 @@
 function objectLiteral <T>  (before: Record<string, T>, after: Record<string, T>)  {
-    const result: Record<string, { old: any; new: any; }> = {};
+    const result: Record<string, { old: T; new: T; }> = {};
     const keyList = Object.keys(before).length > Object.keys(after).length ? Object.keys(before) : Object.keys(after);
     keyList.map(value => {
       if (before[value] !== after[value]) {
-        result[value] = { old: before[value], new: after[value]}
+        result[value] = { old: before[value], new: after[value] }
       }
     })
     return result;
